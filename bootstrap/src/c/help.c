9b9fbdfa0e4ccb4577f193d1ae5068e6c197082d
awewsomegamer <awewsomegamer@gmail.com>
2023-09-03 18:07:30 -0700

    Iterating through tags
    Currently trying to iterate through all of the tags, but I am getting mixed
    results, so it would probably be better to create a simple print
    mechanism so I can see information on the screen, or in my terminal.

--- bootstrap/src/c/help.c
+++ bootstrap/src/c/help.c
@@ -1,15 +1,39 @@
 #include <stdint.h>
 #include <stddef.h>
+#include "include/multiboot2.h"
 
-struct multiboot2_info {
+uint32_t *tags = NULL;
+uint32_t *tags_end = NULL;
+uint32_t cur_tag_sz = 0;
 
-}__attribute__((packed));
+int get_tag() {
+	// We reached the end, return -1
+	if (tags >= tags_end || *tags == MULTIBOOT_TAG_TYPE_END)
+		return -1;
 
-int helper(struct multiboot2_info *boot_info, uint32_t magic) {
+	
+	cur_tag_sz = *(tags + 4);
+
+	// Return tag type
+	return (*tags);
+}
+
+int helper(uint32_t *boot_info, uint32_t magic) {
 	if (magic != 0x36D76289)
 		return 1;
 
-	// *((uint8_t *)0xB8000) = 'A' + boot_info->mods_count;
+	uint32_t total_size = *boot_info;
+	tags_end = boot_info + total_size;
+	tags = boot_info + 8;
+
+	uint8_t *screen = (uint8_t *)0xB8000;
+	int tag = 0;
+	while ((tag = get_tag()) > 0) {
+		*screen = 'A';
+		screen += 2;
+
+		tags += cur_tag_sz;
+	}
 
 	return 0;
 }
