2a11c3b349a65dfb409906c728bcd29854d11e8b
awewsomegamer <awewsomegamer@gmail.com>
2023-11-05 18:31:39 -0800

    Memory management
    Finished with the page frame allocator, currently trying to wrap my
    head around how I should get to work with the virtual memory manager
    and an allocator. Started work on the virtual memory manager. Looking
    at some other operating systems, vmm.c is typically responsible for
    managing the page tables. So far, my vmm.c contains a stack of memory
    ranges. It works on the premise that the first element is located in the
    middle, and there are two hemispheres. Allocations alternate from the
    positive hemisphere to the negative hemisphere. This would hold the
    structure of virtual memory, allowing for mapping to be easier, but I'm
    at an impass, my method or a proven method.

--- kernel/src/c/include/mm/vmm.h
+++ kernel/src/c/include/mm/vmm.h
@@ -5,4 +5,13 @@
 // and which are not. Provide functions for allocating and freeing
 // addresses.
 
+#define VMM_RANGE_TABLE_MAX 4096
+
+#include <global.h>
+
+void *vmm_allocate(size_t size);
+void *vmm_free(void *address, size_t size);
+
+void initialize_vmm();
+
 #endif
--- kernel/src/c/mm/vmm.c
+++ kernel/src/c/mm/vmm.c
@@ -1,2 +1,105 @@
 #include <mm/vmm.h>
 
+#define VMM_RANGE_TABLE_MID (VMM_RANGE_TABLE_MAX / 2)
+
+#define VMM_RANGE_FLAG_USED (1 << 0)
+
+struct vmm_range {
+	uint64_t base;
+	size_t length;
+	uint8_t flags;
+};
+
+static struct vmm_range range_table[VMM_RANGE_TABLE_MAX];
+int allocations = 0;
+
+void *vmm_allocate(size_t size) {
+	int i = 0;
+	uint64_t free_space = 0;
+
+	for (; i < (allocations / 2) + (allocations % 2); i++) {
+		struct vmm_range range = range_table[VMM_RANGE_TABLE_MID + i];
+
+check_hemisphere:
+		if ((range.flags & VMM_RANGE_FLAG_USED) == 0 && range.length >= size) {
+			free_space = range.base;
+			range.flags |= VMM_RANGE_FLAG_USED;
+
+			break;
+		}
+
+		// Reached a brand new, untouched entry
+		// Check complementary ranges for free
+		// space.
+		if (range.length == 0) {
+			free_space = 0x0;
+		}
+
+		// Keep track of free space.
+		
+
+		// Flip i around and check the other
+		// hemisphere.
+		i *= -1;
+
+		if (i < 0) {
+			goto check_hemisphere;
+		}
+	}
+
+	if (allocations == 0) {
+		range_table[VMM_RANGE_TABLE_MID].base = free_space;
+		range_table[VMM_RANGE_TABLE_MID].length = ALIGN(size, PAGE_SIZE);
+	}
+
+	if ((allocations % 2) == 1) {
+		// Insert on the left hand side
+	} else {
+		// Insert on the right hand side
+	}
+
+	allocations++;
+
+	return NULL;
+}
+
+void *vmm_use(void *address, size_t size) {
+	int i = 0;
+
+	for (; i < allocations; i++) {
+		struct vmm_range range = range_table[VMM_RANGE_TABLE_MID + i];
+		if (range.length == 0) {
+			break;
+		}
+	}
+
+	if (allocations == 0) {
+		range_table[VMM_RANGE_TABLE_MID].base = (uintptr_t)address;
+		range_table[VMM_RANGE_TABLE_MID].length = ALIGN(size, PAGE_SIZE);
+
+		return address;
+	}
+
+	if ((allocations % 2) == 1) {
+		// Insert on the left hand side
+
+	} else {
+		// Insert on the right hand side
+	}
+
+	allocations++;
+
+	return address;
+}
+
+void *vmm_free(void *address, size_t size) {
+
+
+	return NULL;
+}
+
+void initialize_vmm() {
+
+
+
+}
