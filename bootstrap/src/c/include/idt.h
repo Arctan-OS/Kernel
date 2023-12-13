06dc57cbb4d0566813e155cca8176cf6a7b84a23
awewsomegamer <awewsomegamer@gmail.com>
2023-12-12 21:45:26 -0800

    Meant to go with last commit

--- bootstrap/src/c/include/idt.h
+++ bootstrap/src/c/include/idt.h
@@ -2,7 +2,9 @@
     Arctan - Operating System Kernel
     Copyright (C) 2023  awewsomegamer
 
-    This program is free software; you can redistribute it and/or
+    This file is apart of Arctan.
+
+    Arctan is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
     as published by the Free Software Foundation; version 2
 
